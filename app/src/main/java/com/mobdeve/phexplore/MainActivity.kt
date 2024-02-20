f4c5e550d51a44cdad6b5bb812e0a23f8960f13e
CodeKiyo <calebrubio175@gmail.com>
2024-02-21 00:44:46 +0800

    Intro (C) + Signup (WIP)

--- app/src/main/java/com/mobdeve/phexplore/MainActivity.kt
+++ app/src/main/java/com/mobdeve/phexplore/MainActivity.kt
@@ -1,12 +1,63 @@
 package com.mobdeve.phexplore
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.UnderlineSpan
+import android.util.Log
+import com.mobdeve.phexplore.databinding.IntroPageBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    // RNG Background Image for intro_page.xml
+    private val backgroundImages = arrayOf(
+        R.drawable.intro_img1,
+        R.drawable.intro_img2,
+        R.drawable.intro_img3
+    )
+
+    private lateinit var introPage : IntroPageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        //testing haha yurrrt ddd
+
+        // ViewBinding
+        introPage = IntroPageBinding.inflate(layoutInflater)
+
+        // RNG Texts for intro_page.xml
+        val quotes = arrayOf(
+            getString(R.string.quote1),
+            getString(R.string.quote2),
+            getString(R.string.quote3)
+        )
+
+        // RNG Logic
+        val randomIndexBG = Random.nextInt(backgroundImages.size)
+        introPage.backgroundIntro.setImageResource(backgroundImages[randomIndexBG])
+        val randomIndexQ = Random.nextInt(quotes.size)
+        introPage.quote.text = quotes[randomIndexQ]
+
+        // Accessing getStarted button
+        val getStarted = introPage.getStarted
+
+        // Adding underline in getStarted text
+        val spannableString = SpannableString(getStarted.text)
+        spannableString.setSpan(UnderlineSpan(), 0, getStarted.text.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        introPage.getStarted.text = spannableString
+
+        // Set view for intro_page.xml
+        setContentView(introPage.root)
+
+        // getStarted button taking you to signup_page.xml
+        getStarted.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+
+            intent.putExtra(SignUpActivity.BACKGROUND_RESOURCE_ID, backgroundImages[randomIndexBG])
+
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/mobdeve/phexplore/SignUpActivity.kt
@@ -0,0 +1,39 @@
+package com.mobdeve.phexplore
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.mobdeve.phexplore.databinding.SignupPageBinding
+
+class SignUpActivity : AppCompatActivity() {
+    companion object{
+        const val BACKGROUND_RESOURCE_ID = "BACKGROUND_RESOURCE_ID"
+    }
+
+    private lateinit var signupPage: SignupPageBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // ViewBinding
+        signupPage = SignupPageBinding.inflate(layoutInflater)
+
+        // Set view for signup_page.xml
+        setContentView(signupPage.root)
+
+        // Accepting the Intent from MainActivity
+        val intentFromMainActivity = intent
+
+        // Getting the Background Drawable
+        val backgroundImage = intentFromMainActivity.getIntExtra(BACKGROUND_RESOURCE_ID, 0)
+
+        if(backgroundImage != 0){
+            signupPage.backgroundSignup.setImageResource(backgroundImage)
+        }else {
+            // Handle the case where no background resource ID is passed
+            // You can set a default background or show an error message
+            Log.e("SignUpActivity", "No background resource ID provided")
+        }
+    }
+}

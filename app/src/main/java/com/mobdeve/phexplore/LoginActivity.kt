728b6379b5fa656c53fc1860cef5063e82c6b91a
CodeKiyo <calebrubio175@gmail.com>
2024-02-24 15:01:10 +0800

    Finished Sign Up + Login WIP

--- app/src/main/java/com/mobdeve/phexplore/LoginActivity.kt
+++ app/src/main/java/com/mobdeve/phexplore/LoginActivity.kt
@@ -1,5 +1,41 @@
 package com.mobdeve.phexplore
 
-class LoginActivity {
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.mobdeve.phexplore.databinding.LoginPageBinding
+import kotlin.math.log
+
+class LoginActivity : AppCompatActivity() {
     // redirect ko yung main activity to login activity instead of signup activity soon
+    companion object{
+        const val BACKGROUND_RESOURCE_ID = "BACKGROUND_RESOURCE_ID"
+    }
+
+    private lateinit var loginPage: LoginPageBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // ViewBinding
+        loginPage = LoginPageBinding.inflate(layoutInflater)
+
+
+        // Set view for signup_page.xml
+        setContentView(loginPage.root)
+
+        // Accepting the Intent from MainActivity
+        val intentFromMainActivity = intent
+
+        // Getting the Background Drawable
+        val backgroundImage = intentFromMainActivity.getIntExtra(SignUpActivity.BACKGROUND_RESOURCE_ID, 0)
+
+        if(backgroundImage != 0){
+            // loginPage.backgroundSignup.setImageResource(backgroundImage)
+        }else {
+            // Handle the case where no background resource ID is passed
+            // You can set a default background or show an error message
+            Log.e("SignUpActivity", "No background resource ID provided")
+        }
+    }
 }
--- app/src/main/java/com/mobdeve/phexplore/SignUpActivity.kt
+++ app/src/main/java/com/mobdeve/phexplore/SignUpActivity.kt
@@ -1,14 +1,11 @@
 package com.mobdeve.phexplore
 
-import android.content.Intent
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.UnderlineSpan
 import android.util.Log
-import android.util.TypedValue
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.core.content.res.ResourcesCompat
-import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
 import com.mobdeve.phexplore.databinding.SignupPageBinding
 
 class SignUpActivity : AppCompatActivity() {
@@ -23,6 +20,22 @@ class SignUpActivity : AppCompatActivity() {
 
         // ViewBinding
         signupPage = SignupPageBinding.inflate(layoutInflater)
+
+        // Accessing signup_login_switch button
+        val signupLoginSwitch = signupPage.signupLoginSwitch
+
+        // Adding underline in signup_login_switch text
+        val spannableString1 = SpannableString(signupLoginSwitch.text)
+        spannableString1.setSpan(UnderlineSpan(), 0, signupLoginSwitch.text.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        signupLoginSwitch.text = spannableString1
+
+        // Accessing signup_skip textview
+        val signupSkip = signupPage.signupSkip
+
+        // Adding underline in signup_login_switch text
+        val spannableString2 = SpannableString(signupSkip.text)
+        spannableString2.setSpan(UnderlineSpan(), 0, signupSkip.text.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        signupSkip.text = spannableString2
 
         // Set view for signup_page.xml
         setContentView(signupPage.root)

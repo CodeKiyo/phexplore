80de77688cf3eb36ed53e5a992571e440160e9ef
rpineda26 <91407317+rpineda26@users.noreply.github.com>
2024-03-07 18:13:14 +0800

    init user page

---
+++ app/src/main/java/com/mobdeve/phexplore/UserPage.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.phexplore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_page)
+    }
+}

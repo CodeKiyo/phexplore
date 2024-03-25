0063183150f9d45cfc671c9125459211dd8c42a8
francoooG <136312811+francoooG@users.noreply.github.com>
2024-03-25 16:26:22 +0800

    Fixed menu item view scroll

--- app/src/main/java/com/mobdeve/phexplore/DestinationViewHolder.kt
+++ app/src/main/java/com/mobdeve/phexplore/DestinationViewHolder.kt
@@ -12,15 +12,18 @@ class DestinationViewHolder(itemView:View): RecyclerView.ViewHolder(itemView) {
     private val name: TextView = itemView.findViewById(R.id.destName)
     private val image: ImageView = itemView.findViewById(R.id.destImage)
     private val location: TextView = itemView.findViewById(R.id.destLocation)
+    private val description: TextView? = itemView.findViewById(R.id.destDescription)
     fun bindData(model: DestinationModel) {
         this.name.text = model.destName
         this.image.setImageResource(model.destImage)
         this.location.text = model.destCity
+        this.description?.text = model.destDescription
         this.itemView.setOnClickListener {
             val intentToViewItem = Intent(itemView.context, MenuItemViewActivity::class.java)
             intentToViewItem.putExtra("DEST_NAME", model.destName)
             intentToViewItem.putExtra("DEST_IMAGE", model.destImage)
             intentToViewItem.putExtra("DEST_CITY", model.destCity)
+            intentToViewItem.putExtra("DEST_DESCRIPTION", model.destDescription)
             (itemView.context as? Activity)?.startActivity(intentToViewItem, null)
         }
     }

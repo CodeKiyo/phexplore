99a8e03a5308e90a7f9d0516c29d525bca6fc196
francoooG <136312811+francoooG@users.noreply.github.com>
2024-03-30 02:04:28 +0800

    Update FilterViewHolder.kt

--- app/src/main/java/com/mobdeve/phexplore/FilterViewHolder.kt
+++ app/src/main/java/com/mobdeve/phexplore/FilterViewHolder.kt
@@ -22,30 +22,51 @@ class FilterViewHolder(itemView: View, private val verticalRecyclerView: Recycle
         this.filter_name.text = model.filterName
         this.filter_image.setImageResource(model.filterImage)
         this.filter_cardcolor.backgroundTintList = ColorStateList.valueOf(Color.parseColor(model.filterColor))
+        val db = Firebase.firestore
+        val destinationsRef = db.collection(MyFirestoreReferences.DESTINATIONS_COLLECTION)
+        val allData = ArrayList<DestinationModel>()
+
+        val destName = MyFirestoreReferences.DESTNAME_FIELD
+        val destDescription = MyFirestoreReferences.DESTDESCRIPTION_FIELD
+        val destCity = MyFirestoreReferences.DESTCITY_FIELD
+        val destImage = MyFirestoreReferences.DESTIMAGE_FIELD
+        val destCategory = MyFirestoreReferences.DESTCATEGORY_FIELD
+        destinationsRef.get().addOnSuccessListener { result ->
+            for (document in result!!.documents) {
+                val newData = DestinationModel(
+                    document.get(destName).toString(),
+                    document.get(destDescription).toString(),
+                    document.get(destImage).toString(),
+                    document.get(destCity).toString(),
+                    document.get(destCategory).toString())
+                allData.add(newData)
+                Log.d("FilterViewHolder if", allData.size.toString())
+            }
+        }.addOnFailureListener { exception ->
+            println("Error getting documents: $exception")
+        }
+
 
         this.itemView.setOnClickListener {
-            val db = Firebase.firestore
-            val destinationsRef = db.collection(MyFirestoreReferences.DESTINATIONS_COLLECTION)
-            val data = ArrayList<DestinationModel>()
-            val destName = MyFirestoreReferences.DESTNAME_FIELD
-            val destDescription = MyFirestoreReferences.DESTDESCRIPTION_FIELD
-            val destCity = MyFirestoreReferences.DESTCITY_FIELD
-            val destImage = MyFirestoreReferences.DESTIMAGE_FIELD
-            val destCategory = MyFirestoreReferences.DESTCATEGORY_FIELD
+            var filteredData = ArrayList<DestinationModel>()
             destinationsRef.get().addOnSuccessListener { result ->
                 for (document in result!!.documents) {
                     val condition = document.get(destCategory).toString()
-                    if(condition == this.filter_name.text) {
+                    if (condition == this.filter_name.text && condition != "All") {
                         val newData = DestinationModel(
                             document.get(destName).toString(),
                             document.get(destDescription).toString(),
                             document.get(destImage).toString(),
                             document.get(destCity).toString(),
                             document.get(destCategory).toString())
-                        data.add(newData)
+                        filteredData.add(newData)
+                    } else if(this.filter_name.text == "All") {
+                        filteredData = allData
+                        verticalRecyclerView.adapter = DestinationAdapter(filteredData, 1)
+                        return@addOnSuccessListener
                     }
                 }
-                verticalRecyclerView.adapter = DestinationAdapter(data, 1)
+                verticalRecyclerView.adapter = DestinationAdapter(filteredData, 1)
             }.addOnFailureListener { exception ->
                 println("Error getting documents: $exception")
             }

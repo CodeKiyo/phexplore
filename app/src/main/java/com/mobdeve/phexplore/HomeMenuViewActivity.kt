764300bdec886007aa51864ad13ac8e428f13027
francoooG <136312811+francoooG@users.noreply.github.com>
2024-03-31 19:47:11 +0800

    logout popup function

--- app/src/main/java/com/mobdeve/phexplore/HomeMenuViewActivity.kt
+++ app/src/main/java/com/mobdeve/phexplore/HomeMenuViewActivity.kt
@@ -3,6 +3,7 @@ package com.mobdeve.phexplore
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.mobdeve.phexplore.databinding.HomemenuPageBinding
@@ -38,9 +39,24 @@ class HomeMenuViewActivity : AppCompatActivity()  {
                 R.id.bottom_settings -> {
                     // start activity
                     overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
-                    val intentToLoginPage = Intent(this, LoginActivity::class.java)
-                    startActivity(intentToLoginPage)
-                    finish()
+                    if(username != "Guest") {
+                        val builder = AlertDialog.Builder(this)
+                        builder.setMessage("Are you sure you want to log out?")
+                        builder.setCancelable(true)
+                        builder.setPositiveButton("Yes") { dialog, id ->
+                            val intentToLoginPage = Intent(this, LoginActivity::class.java)
+                            startActivity(intentToLoginPage)
+                            finish()
+                        }
+                        builder.setNegativeButton("No") { dialog, id -> dialog.cancel() }
+
+                        val alert = builder.create()
+                        alert.show()
+                    } else {
+                        val intentToLoginPage = Intent(this, LoginActivity::class.java)
+                        startActivity(intentToLoginPage)
+                        finish()
+                    }
                     // finish
                     true
                 }
